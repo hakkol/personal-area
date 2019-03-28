8fb600969f7ccb552b43823e2a699d99d7225516
Николай Аблов <ablov@a2design.biz>
2019-03-28 10:13:29 +0600

    Created RolesTableSeeder and added in DatabaseSeeder

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Carbon\Carbon;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            [
+                'name'       => 'admin',
+                'created_at' =>  Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'name'       => 'user',
+                'created_at' =>  Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ]);
+    }
+}
