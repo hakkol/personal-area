3bfd45fa67aaadd4bc529ccb460b31caf63d9822
Николай Аблов <ablov@a2design.biz>
2019-03-29 14:39:15 +0600

    Created user/products folder with index view

---
+++ resources/views/user/products/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+
+@section('title')
+    Products
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" >
+                <h2>Products</h2>
+            </div>
+
+            <div class="col-md-12">
+                @if ($products->isNotEmpty())
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Cost</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <td>{{ $product->name }}</td>
+                                    <td>{{ $product->cost }}</td>
+                                    <td class="text-center">
+                                        <button class="btn btn-success" type="button">Buy</button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <div class="d-flex justify-content-center">
+                        {!! $products->render() !!}
+                    </div>
+                @else
+                    <h2>Products not found</h2>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
