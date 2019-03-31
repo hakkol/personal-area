c9835c6d081309bc3637a0164ceac27689abd9e5
Nikolay Ablov <ablov@a2design.biz>
2019-03-31 14:15:58 +0600

    Added "Orders" link in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -38,6 +38,11 @@
                             Products
                         </a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ action('Admin\OrderController@index') }}">
+                            Orders
+                        </a>
+                    </li>
                 </ul>
 
                 <!-- Right Side Of Navbar -->
