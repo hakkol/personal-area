28ddaf0c1ee7b940cf076d14ae69c472c2571fcb
Nikolay Ablov <ablov@a2design.biz>
2019-03-31 12:03:25 +0600

    Added casts, status relationship, mutators (product_name, product_cost and status_name), checkDataAtttribute method in Order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,73 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
+
+    /**
+    * Get the status associated with the given order.
+    *
+    * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+    */
+    public function status()
+    {
+
+        return $this->belongsTo('App\Models\Status');
+    }
+
+    /**
+     * Get product name from order
+     *
+     * @return string  name
+     */
+    public function getProductNameAttribute()
+    {
+
+        return $this->checkDataAttribute('product_name');
+    }
+
+    /**
+     * Get product name from order
+     *
+     * @return string  name
+     */
+    public function getProductCostAttribute()
+    {
+
+        return $this->checkDataAttribute('product_cost');
+    }
+
+    /**
+     * Get status name from order
+     *
+     * @return string  name
+     */
+    public function getStatusNameAttribute()
+    {
+        $status = $this->status;
+
+        if (!$status) return '';
+
+        return $status->name;
+    }
+
+    /**
+     * Get data value
+     *
+     * @return string  data value
+     */
+    public function checkDataAttribute($name)
+    {
+        $data = $this->data;
+
+        if (isset($data[$name])) return $data[$name];
+
+        return '';
+    }
 }
